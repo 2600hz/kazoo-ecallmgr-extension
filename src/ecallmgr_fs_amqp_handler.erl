cbbe13f292dd59cee937c25eb66dfa75738f8069
lazedo <luis.azedo@factorlusitano.com>
2015-11-20 23:54:06 +0000

    OTP18 and process tweak

--- src/ecallmgr_fs_amqp_handler.erl
+++ src/ecallmgr_fs_amqp_handler.erl
@@ -16,19 +16,24 @@ init() -> 'ok'.
 
 -spec handle_req(wh_json:object(), wh_proplist()) -> 'ok'.
 handle_req(JObj, Props) ->
+    Node = props:get_value('FSNode', Props),
     UUID = wh_json:get_value(<<"Unique-ID">>, JObj),
     Event = wh_json:get_value(<<"Event-Subclass">>, JObj, wh_json:get_value(<<"Event-Name">>, JObj)),
     EventProps = [{<<"Switch-URL">>, props:get_value('Switch-URL', Props)}
                   ,{<<"Switch-URI">>, props:get_value('Switch-URI', Props)}
                   | wh_json:to_proplist(JObj)
                  ],
-    process_event(UUID, Event, EventProps).
+    process_event(Node, UUID, Event, EventProps).
 
-process_event(UUID, <<"CHANNEL_DESTROY">> = Event, Props) ->
+process_event(Node, UUID, <<"CHANNEL_CREATE">> = Event, Props) ->
+    NewProps = ecallmgr_fs_loopback:filter(Node, UUID, Props),
+    lager:debug("processing freeswitch amqp event ~s for callid ~s", [Event, UUID]),
+    ecallmgr_call_events:process_channel_event(NewProps);
+process_event(_Node, UUID, <<"CHANNEL_DESTROY">> = Event, Props) ->
     lager:debug("processing freeswitch amqp event ~s for callid ~s", [Event, UUID]),
     ecallmgr_call_events:process_channel_event(Props);
-process_event(UUID, <<"CHANNEL_HANGUP_COMPLETE">> = Event, _Props) ->
+process_event(_Node, UUID, <<"CHANNEL_HANGUP_COMPLETE">> = Event, _Props) ->
     lager:debug("ignoring freeswitch amqp event ~s for callid ~s", [Event, UUID]);
-process_event(UUID, Event, Props) ->
+process_event(_Node, UUID, Event, Props) ->
     lager:debug("processing freeswitch amqp event ~s for callid ~s", [Event, UUID]),
     ecallmgr_call_events:process_channel_event(Props).
