e1ebe14fa607f7174291ae4df4f6880a21bafa36
lazedo <luis.azedo@factorlusitano.com>
2020-12-23 03:33:27 +0000

    Node restarts (#41)
    * remove heartbeat as not needed
    
    * refactor to handle several conditions
    
    * ecallmgr may have not received the nodedown
      and thinks node is up
    
    * ecallmgr can have CoreUUID (freeswitch) out of sync
    
    * ecallmgr never lost track of node and will never
      send start cmds (load mod_sofia) even in new CoreUUID
    
    * trigger restart cmds from oldest app node
      if mod_sofia is not loaded

--- src/mod_com_kazoo_monitor.erl
+++ src/mod_com_kazoo_monitor.erl
@@ -114,78 +114,128 @@ is_media_server(#kz_node{kapps=Apps}) ->
 handle_heartbeat(Node, State) ->
     CoreUUID = core_uuid(Node),
     Nodename = node_name(Node),
-    handle_heartbeat(is_media_server(Node), CoreUUID, Nodename, State).
+    handle_heartbeat(is_media_server(Node), Node, CoreUUID, Nodename, State).
 
--spec handle_heartbeat(boolean(), atom(), atom(), state()) -> state().
-handle_heartbeat(false, _CoreUUID, _Nodename, State) -> State;
-handle_heartbeat(true, CoreUUID, Nodename, #{nodes := Nodes} = State) ->
-    INU = ecallmgr_fs_nodes:is_node_up(Nodename),
-    case heartbeat_match(CoreUUID, Nodename, Nodes) of
-        {#{name := _}, _} when INU ->
-            _ = maybe_update_code(Nodes),
-            State;
-        {#{name := _}, _} ->
-            _ = notify_nodeup(Nodename),
-            State;
-        {_, #{uuid := CoreUUID}} when INU ->
-            _ = maybe_update_code(Nodes),
-            State;
-        {_, #{uuid := CoreUUID}} ->
-            _ = notify_nodeup(Nodename),
-            State;
-        {_, #{uuid := OtherUUID}} when INU ->
-            RemovedNodes = maps:without([OtherUUID, Nodename], Nodes),
-            NewNodes = RemovedNodes#{CoreUUID => #{name => Nodename}
-                                    ,Nodename => #{uuid => CoreUUID}
-                                    },
-            _ = update_code(NewNodes),
-            State#{nodes => NewNodes};
-        {_, #{uuid := OtherUUID}} ->
-            RemovedNodes = maps:without([OtherUUID, Nodename], Nodes),
-            NewNodes = RemovedNodes#{CoreUUID => #{name => Nodename}
-                                    ,Nodename => #{uuid => CoreUUID}
-                                    },
-            _ = update_code(NewNodes),
-            _ = notify_nodeup(Nodename),
-            State#{nodes => NewNodes};
-        {undefined, undefined}
-          when INU ->
-            NewNodes = Nodes#{CoreUUID => #{name => Nodename}
-                             ,Nodename => #{uuid => CoreUUID}
-                             },
-            _ = update_code(NewNodes),
-            State#{nodes => NewNodes};
-        {undefined, undefined} ->
-            handle_nodeup(CoreUUID, Nodename, State);
-        _Other ->
-            lager:info("unhandled heartbeat match => ~p", [_Other]),
-            State
-    end.
-
--type node_map_result() :: undefined | map().
--type heartbeat_match_result() :: {node_map_result(), node_map_result()}.
-
--spec heartbeat_match(atom(), atom(), map()) -> heartbeat_match_result().
-heartbeat_match(CoreUUID, Nodename, Nodes) ->
-    {maps:get(CoreUUID, Nodes, undefined)
-    ,maps:get(Nodename, Nodes, undefined)
-    }.
-
--spec handle_nodeup(atom(), atom(), state()) -> state().
-handle_nodeup(CoreUUID, Nodename, #{nodes := Nodes}=State) ->
-    NewNodes = Nodes#{CoreUUID => #{name => Nodename}
-                     ,Nodename => #{uuid => CoreUUID}
-                     },
-    _ = update_code(NewNodes),
-    _ = notify_nodeup(Nodename),
+-spec handle_heartbeat(boolean(), kz_types:kz_node(), atom(), atom(), state()) -> state().
+handle_heartbeat(false, _Node, _CoreUUID, _Nodename, State) -> State;
+handle_heartbeat(true, Node, CoreUUID, Nodename, #{nodes := Nodes} = State) ->
+    Context = #{core_uuid => CoreUUID
+               ,node_uuid => ecallmgr_fs_nodes:instance_uuid(Nodename)
+               ,node_up => ecallmgr_fs_nodes:is_node_up(Nodename)
+               ,nodes => Nodes
+               ,node => Node
+               ,nodename => Nodename
+               ,is_node => ecallmgr_fs_nodes:is_node(Nodename)
+               ,node_server => node_server(Nodename)
+               },
+    Routines = [fun update_nodes/1
+               ,fun maybe_update_code/1
+               ,fun maybe_node_up/1
+               ,fun maybe_node_sync/1
+               ,fun maybe_node_run/1
+               ],
+    #{nodes := NewNodes} = kz_maps:exec(Routines, Context),
     State#{nodes => NewNodes}.
 
--spec notify_nodeup(atom()) -> 'ok' | {'error', 'no_connection'}.
-notify_nodeup(Nodename) ->
-    case ecallmgr_fs_nodes:is_node(Nodename) of
-        'true' -> ecallmgr_fs_nodes:nodeup(Nodename, 'heartbeat');
-        'false' -> ecallmgr_fs_nodes:add(Nodename, 'no_cookie', [{'connect_strategy', 'heartbeat'}])
+update_nodes(#{core_uuid := CoreUUID
+              ,nodename := Nodename
+              ,nodes := CurrentNodes
+              } = Context) ->
+    Nodes = maybe_remove_older_uuid(CoreUUID, Nodename, CurrentNodes),
+    Context#{nodes => Nodes#{CoreUUID => #{name => Nodename}
+                            ,Nodename => #{uuid => CoreUUID}
+                            }}.
+
+maybe_remove_older_uuid(CoreUUID, Nodename, Nodes) ->
+    case maps:get(Nodename, Nodes, undefined) of
+        undefined -> Nodes;
+        #{uuid := CoreUUID} -> Nodes;
+        #{uuid := OtherUUID} -> maps:without([OtherUUID], Nodes)
     end.
+
+maybe_update_code(#{nodes := Nodes} = Context) ->
+    Props = [{K, Name} || {K, #{name := Name}} <- maps:to_list(Nodes)],
+    _ = maybe_update_code(Nodes, lists:any(fun is_code_not_handled/1, Props)),
+    Context.
+
+maybe_update_code(Nodes, true) -> update_code(Nodes);
+maybe_update_code(_Nodes, false) -> ok.
+
+maybe_node_up(#{is_node := true
+               ,node_up := true
+               } = Context) ->
+    Context;
+maybe_node_up(#{is_node := true
+               ,node_up := false
+               ,nodename := Nodename
+               } = Context) ->
+    lager:info("notifying node ~s is up", [Nodename]),
+    ecallmgr_fs_nodes:nodeup(Nodename, 'heartbeat'),
+    Context;
+maybe_node_up(#{is_node := false
+               ,nodename := Nodename
+               } = Context) ->
+    lager:info("adding new node ~s", [Nodename]),
+    ecallmgr_fs_nodes:add(Nodename, 'no_cookie', [{'connect_strategy', 'heartbeat'}]),
+    Context.
+
+node_server(Nodename) ->
+    ecallmgr_fs_node_sup:node_srv(Nodename).
+
+maybe_node_sync(#{nodename := Nodename
+                 ,node_server := undefined
+                 } = Context) ->
+    lager:info("node server is down ~s", [Nodename]),
+    Context;
+maybe_node_sync(#{is_node := true
+                 ,core_uuid := CoreUUID
+                 ,nodename := Nodename
+                 ,node_server := Server
+                 } = Context) ->
+    case node_instance_uuid(Server) of
+        CoreUUID ->
+            Context;
+        _OtherUUID ->
+            lager:info("synchronizing node ~s uuid from ~s to ~s", [Nodename, _OtherUUID, CoreUUID]),
+            ecallmgr_fs_node:sync_info(Nodename),
+            Context
+    end;
+maybe_node_sync(Context) -> Context.
+
+node_instance_uuid(Nodename) ->
+    kz_term:to_atom(ecallmgr_fs_node:instance_uuid(Nodename), true).
+
+maybe_node_run(#{nodename := Nodename
+                ,node_server := undefined
+                } = Context) ->
+    lager:info("node server is down ~s", [Nodename]),
+    Context;
+maybe_node_run(#{is_node := true
+                ,nodename := Nodename
+                ,node := #kz_node{modules=Modules}
+                ,node_server := Server
+                } = Context) ->
+    Loaded = kz_json:get_list_value(<<"loaded">>, Modules),
+    case not lists:member(<<"mod_sofia">>, Loaded)
+        andalso should_run_cmds()
+    of
+        false ->
+            Context;
+        true ->
+            lager:info("starting node commands for ~s", [Nodename]),
+            run_node(Server, Nodename),
+            Context
+    end.
+
+run_node(Server, Nodename) ->
+    handle_run_node_result(ecallmgr_fs_node:node_restart(Server), Nodename).
+
+handle_run_node_result({error, invalid_node}, Nodename) ->
+    lager:error("how did this happened? node ~s disapeared from fs nodes", [Nodename]);
+handle_run_node_result(ok, _Nodename) -> ok.
+
+should_run_cmds() ->
+    kz_nodes:whapp_oldest_node(?APP) =:= node().
 
 -spec handle_expired(kz_types:kz_node(), state()) -> state().
 handle_expired(Node, State) ->
@@ -202,23 +252,17 @@ handle_expired(true, CoreUUID, Nodename, #{nodes := Nodes} = State) ->
     _ = ecallmgr_fs_nodes:nodedown(Nodename),
     State#{nodes => NewNodes}.
 
-is_code_handled({CoreUUID, NodeName}) ->
+is_code_not_handled({CoreUUID, NodeName}) ->
     freeswitch:mod(NodeName) =/= 'mod_com_kazoo'
         orelse mod_com_kazoo:core_uuid(NodeName) =/= CoreUUID.
-
-maybe_update_code(Nodes) ->
-    Props = [{K, Name} || {K, #{name := Name}} <- maps:to_list(Nodes)],
-    case lists:any(fun is_code_handled/1, Props) of
-        'true' -> update_code(Nodes);
-        'false' -> 'ok'
-    end.
 
 update_code(Nodes) ->
     lager:info("mod_com_kazoo code out of sync, updating."),
     Props = [{K, Name} || {K, #{name := Name}} <- maps:to_list(Nodes)],
     FSCode = freeswitch_code(Props),
+    MODCode = mod_com_kazoo_code(Props),
     meta:replace_function('mod', 1, FSCode, 'freeswitch'),
-    meta:replace_function('core_uuid', 1, mod_com_kazoo_code(Props), 'mod_com_kazoo').
+    meta:replace_function('core_uuid', 1, MODCode, 'mod_com_kazoo').
 
 freeswitch_code(Props) ->
     {'function',1,'mod',1,
