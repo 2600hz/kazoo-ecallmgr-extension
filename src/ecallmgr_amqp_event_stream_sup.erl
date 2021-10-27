0afeaca2957fc857427bf5469ecb48f37b108557
lazedo <luis@2600hz.com>
2021-10-27 01:05:34 +0100

    amqp config updates (#49)
    * use ecallmgr for app settings
    
    * instead of having ecallmgr_extension document
    
    * add pre_process vars to config
    
    * split create/answer/destroy
    
    * instead of having 3 critical events going thru the same pipe
    
    * modify mod_com_kazoo configuration profiles
    
    * splits channel create/answer/destroy
    * adds workers for processing events
    
    * preserved the 5.0 use_proxy_contact_api default

--- src/ecallmgr_amqp_event_stream_sup.erl
+++ src/ecallmgr_amqp_event_stream_sup.erl
@@ -36,7 +36,7 @@ start_link({Name, _}=Profile) ->
     lager:debug("starting amqp event stream supervisor ~s : ~p", [Name, Server]),
     {'ok', Pid} = supervisor:start_link({'local', Server}, ?MODULE, [Profile]),
     lager:debug("started amqp event stream supervisor ~s : ~p", [Name, Pid]),
-    Workers = kz_app_config:get_integer(?APP, [<<"amqp_event_stream">>, kz_term:to_binary(Name)], 1),
+    Workers = kz_app_config:get_integer(?APP_CONFIG_CAT, [<<"amqp">>, <<"event_stream">>, kz_term:to_binary(Name)], 1),
     kz_process:spawn(fun() -> [begin
                                    timer:sleep(500),
                                    supervisor:start_child(Pid, [Profile])
--- src/mod_com_kazoo.erl
+++ src/mod_com_kazoo.erl
@@ -403,7 +403,7 @@ async_api(Node, Cmd, Args) ->
 
 -spec contact_api() -> binary().
 contact_api() ->
-    case kz_app_config:get_boolean(?APP, <<"use_proxy_contact_api">>, false) of
+    case kz_app_config:get_boolean(?APP_CONFIG_CAT, <<"use_proxy_contact_api">>, false) of
         true -> <<"kz_proxy_contact">>;
         false -> <<"kz_contact">>
     end.
