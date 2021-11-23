7d52d4ff4acd50c86bb33753173c647f26e960fd
Luis Azedo <luis.azedo@factorlusitano.com>
2021-11-23 16:47:47 +0000

    add api correlation
    * since amqp is async we need to have a correlation to ensure the response goes to proper instance of caller

--- src/mod_com_kazoo_api.erl
+++ src/mod_com_kazoo_api.erl
@@ -25,6 +25,8 @@
         ,init/1
         ]).
 
+-export([direct_api_q_strategy/0, set_direct_api_q_strategy/1]).
+
 -type state() :: map().
 
 %% ===================================================================
@@ -49,10 +51,11 @@ start_link() ->
 %%------------------------------------------------------------------------------
 -spec init(list()) -> {'ok', state()}.
 init(_) ->
-    Workers = kz_app_config:get_integer(?APP,[<<"amqp">>, <<"api">>, <<"listeners">>], 5),
-    gen_server:cast(self(), init),
+    Workers = kz_app_config:get_integer(?APP_CONFIG_CAT,[<<"amqp">>, <<"api">>, <<"listeners">>], 5),
+    QueueStrategy = kz_app_config:get_atom(?APP_CONFIG_CAT, [<<"amqp">>, <<"api">>, <<"queue_strategy">>], private),
+    kz_amqp_channel:requisition(),
     {'ok', #{workers => Workers
-            ,queue => set_queue()
+            ,queue => set_queue(QueueStrategy)
             ,listeners => #{}
             ,pids => #{}
             ,channels => #{}
@@ -73,15 +76,11 @@ handle_call(_Request, _From, State) ->
 %% @end
 %%------------------------------------------------------------------------------
 -spec handle_cast(any(), state()) -> kz_types:handle_cast_ret_state(state()).
-handle_cast('init', State) ->
-    kz_amqp_channel:requisition(),
-    {'noreply', init_queues(State)};
-
 handle_cast('init_queues', State) ->
     {'noreply', init_queues(State)};
 
-handle_cast({'com_kazoo_api_listener_is_ready', Pid, Channel}, State) ->
-    {'noreply', add_listener(Pid, Channel, State)};
+handle_cast({'com_kazoo_api_listener_is_ready', Pid, Channel, Queue, Active}, State) ->
+    {'noreply', add_listener(Pid, Channel, Queue, Active, State)};
 
 handle_cast(_Msg, State) ->
     lager:debug("unhandled cast: ~p", [_Msg]),
@@ -125,45 +124,51 @@ code_change(_OldVsn, State, _Extra) ->
     {'ok', State}.
 
 
--spec remove_listener(pid(), pid(), state()) -> state().
-remove_listener(Pid, Channel, State) ->
+-spec remove_listener(pid(), state()) -> state().
+remove_listener(Pid, State) ->
     #{listeners := Listeners, channels := Channels, refs := Refs} = State,
     case maps:get(Pid, Listeners, undefined) of
         undefined -> State;
-        #{channel := Channel} -> State;
-        #{channel := OldChannel, monitor := ListenerMonitor} ->
+        #{channel := Channel, monitor := ListenerMonitor} ->
             erlang:demonitor(ListenerMonitor),
-            #{monitor := ChannelMonitor} = maps:get(OldChannel, Channels),
+            #{monitor := ChannelMonitor} = maps:get(Channel, Channels),
             erlang:demonitor(ChannelMonitor),
             NewRefs = maps:without([ListenerMonitor, ChannelMonitor], Refs),
             NewListeners = maps:without([Pid], Listeners),
-            NewChannels = maps:without([OldChannel], Channels),
+            NewChannels = maps:without([Channel], Channels),
             State#{refs => NewRefs, listeners => NewListeners, channels => NewChannels}
-    end.
+end.
 
--spec add_listener(pid(), pid(), state()) -> state().
-add_listener(Pid, Channel, State0) ->
-    State = remove_listener(Pid, Channel, State0),
+-spec add_listener(pid(), pid(), kz_term:ne_binary(), boolean(), state()) -> state().
+add_listener(Pid, Channel, Queue, Active, State0) ->
+    State = remove_listener(Pid, State0),
     #{listeners := Listeners, channels := Channels, refs := Refs} = State,
 
-    PidRef = erlang:monitor(process, Pid),
+    NewListeners = maps:put(Pid, #{channel => Channel, queue => Queue}, maps:without([Pid], Listeners)),
+    NewChannels = maps:put(Channel, #{listener => Pid, queue => Queue}, maps:without([Channel], Channels)),
+
+    ListenerRef = erlang:monitor(process, Pid),
     ChannelRef = erlang:monitor(process, Channel),
-    NewRefs0 = maps:put(PidRef, #{listener => Pid}, Refs),
+    NewRefs0 = maps:put(ListenerRef, #{listener => Pid}, Refs),
     NewRefs = maps:put(ChannelRef, #{channel => Channel}, NewRefs0),
 
-    NewListeners = maps:put(Pid, #{channel => Channel, monitor => PidRef}, Listeners),
-    NewChannels = maps:put(Channel, #{pid => Pid, monitor => ChannelRef}, Channels),
+    case Active of
+        true -> set_api_refs(NewChannels);
+        false -> set_api_refs(maps:without([Channel], NewChannels))
+    end,
 
-    set_channels(maps:keys(NewChannels)),
     maybe_start_monitor(),
+
     State#{listeners => NewListeners, channels => NewChannels, refs => NewRefs}.
 
 maybe_start_monitor() ->
-    maybe_start_monitor(whereis(mod_com_kazoo_monitor)).
-maybe_start_monitor(undefined) ->
+    maybe_start_monitor(whereis(mod_com_kazoo_monitor), api_refs()).
+
+maybe_start_monitor(undefined, []) -> ok;
+maybe_start_monitor(undefined, _Refs) ->
     _ = mod_com_kazoo_sup:start_monitor(),
     ok;
-maybe_start_monitor(_) -> ok.
+maybe_start_monitor(_, _) -> ok.
 
 -spec init_queues(state()) -> state().
 init_queues(#{workers := Workers, queue := Queue} = State) ->
@@ -183,25 +188,47 @@ start_listeners(Workers, Queue, Self) ->
 start_listener(Self, Queue) ->
     mod_com_kazoo_listener_sup:start_listener(Self, Queue).
 
-
-
-set_queue() ->
+-spec set_queue(atom()) -> kz_term:api_ne_binary().
+set_queue(private) ->
+    persistent_term:put(mod_com_kazoo_api_amqp_queue, undefined),
+    undefined;
+set_queue(shared) ->
     Queue = list_to_binary([<<"com-kazoo-api-">>, kz_binary:rand_uuid()]),
     persistent_term:put(mod_com_kazoo_api_amqp_queue, Queue),
-    Queue.
+    Queue;
+set_queue(_) ->
+    persistent_term:put(mod_com_kazoo_api_amqp_queue, undefined),
+    undefined.
 
--spec queue() -> kz_term:ne_binary().
-queue() -> persistent_term:get(mod_com_kazoo_api_amqp_queue).
+-spec set_direct_api_q_strategy(atom()) -> ok.
+set_direct_api_q_strategy(Strategy) ->
+    persistent_term:put(mod_com_kazoo_api_q_strategy, Strategy).
+
+-spec direct_api_q_strategy() -> atom().
+direct_api_q_strategy() ->
+    persistent_term:get(mod_com_kazoo_api_q_strategy, sequential).
+
 
 -type api_publish_fun() :: fun((kz_term:api_terms()) -> any()).
+-type api_ref() :: {pid(), kz_term:ne_binary()} | undefined.
 
--spec send(kz_term:api_terms(), api_publish_fun()) -> 'ok'.
+-spec send(kz_term:api_terms(), api_publish_fun()) -> {error, nodedown} | kz_term:ne_binary().
 send(Payload, PublisherFun) ->
-    Channel = consumer_channel(),
-    kz_amqp_channel:consumer_channel(channel()),
-    PublisherFun([{<<"Server-ID">>, queue()} | Payload]),
+    send(Payload, PublisherFun, direct_api_ref(direct_api_q_strategy())).
+
+-spec send(kz_term:api_terms(), api_publish_fun(),api_ref()) -> {error, nodedown} | kz_term:ne_binary().
+send(_Payload, _PublisherFun, undefined) ->
+    {error, nodedown};
+send(Payload, PublisherFun, {Channel, Queue}) ->
+    MsgId = kz_binary:rand_uuid(),
+    Values = [{<<"Server-ID">>, Queue}
+             ,{<<"Msg-ID">>, MsgId}
+             ],
+    ClientChannel = consumer_channel(),
     kz_amqp_channel:consumer_channel(Channel),
-    'ok'.
+    PublisherFun(props:set_values(Values, Payload)),
+    kz_amqp_channel:consumer_channel(ClientChannel),
+    MsgId.
 
 consumer_channel() ->
     consumer_channel(kz_amqp_channel:has_channel()).
@@ -223,22 +250,36 @@ next() ->
     counters:add(counter(), 1, 1),
     counters:get(counter(), 1).
 
-set_channels(Channels) ->
-    persistent_term:put(mod_com_kazoo_api_channels, Channels).
+-spec set_api_refs(map() | undefined) -> ok.
+set_api_refs(undefined) ->
+    persistent_term:put(mod_com_kazoo_api_refs, []);
+set_api_refs(Channels) ->
+    persistent_term:put(mod_com_kazoo_api_refs, maps:fold(fun build_api_ref/3, [], Channels)).
 
-channels() ->
-    persistent_term:get(mod_com_kazoo_api_channels).
+build_api_ref(Channel, #{queue := Queue}, Acc)->
+    [{Channel, Queue} | Acc].
 
-channel() ->
-    Channels = channels(),
-    Index = next() rem length(Channels),
-    lists:nth(Index + 1, Channels).
+api_refs() ->
+    persistent_term:get(mod_com_kazoo_api_refs).
+
+
+-spec direct_api_ref(atom()) -> api_ref().
+direct_api_ref(Strategy) ->
+    direct_api_ref(Strategy, api_refs()).
+
+direct_api_ref(_, []) -> undefined;
+direct_api_ref(random, APIRefs) ->
+    Index = rand:uniform(length(APIRefs)),
+    lists:nth(Index, APIRefs);
+direct_api_ref(sequential, APIRefs) ->
+    Index = next() rem length(APIRefs),
+    lists:nth(Index + 1, APIRefs).
 
 handle_canary(false, Channel, State) ->
     gen_server:cast(self(), 'init_queues'),
     handle_canary(Channel, State);
 handle_canary(true, Channel, #{channels := Channels} = State) ->
-    set_channels(maps:keys(Channels)),
+    set_api_refs(Channels),
     handle_canary(Channel, State).
 
 handle_canary(Channel, #{refs := Refs} = State) ->
@@ -258,7 +299,7 @@ handle_down(Pid, Ref, Reason, #{refs := Refs} = State) ->
 
 handle_down(Pid, Ref, Reason, #{canary := Pid}, #{refs := Refs, canary := Pid} = State) ->
     lager:debug("received down (~p/~p/~p) for canary channel, we're closing until we get it back", [Pid, Ref, Reason]),
-    set_channels([]),
+    set_api_refs(undefined),
     NewRefs = maps:without([Ref], Refs),
     State#{refs => NewRefs};
 handle_down(Pid, Ref, Reason, #{listener := Pid}, #{refs := Refs, listeners := Listeners, channels := Channels} = State) ->
@@ -267,11 +308,11 @@ handle_down(Pid, Ref, Reason, #{listener := Pid}, #{refs := Refs, listeners := L
     NewRefs = maps:without([Ref], Refs),
     NewListeners = maps:without([Pid], Listeners),
     NewChannels = maps:without([Channel], Channels),
-    set_channels(NewChannels),
+    set_api_refs(NewChannels),
     State#{refs => NewRefs, listeners => NewListeners, channels => NewChannels};
 handle_down(Pid, Ref, Reason, #{channel := Pid}, #{refs := Refs, channels := Channels} = State) ->
     lager:warning("received down (~p/~p/~p) for channel, this is bad.", [Pid, Ref, Reason]),
     NewRefs = maps:without([Ref], Refs),
     NewChannels = maps:without([Pid], Channels),
-    set_channels(NewChannels),
+    set_api_refs(NewChannels),
     State#{refs => NewRefs, channels => NewChannels}.
