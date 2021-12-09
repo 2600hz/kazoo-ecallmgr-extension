da1c6e42e9a4e406692f4c6bf4b4b02dbf90d244
lazedo <luis@2600hz.com>
2021-12-09 19:59:28 +0000

    version config (backport of #57) (#58)
    * add application version and timestamp to configuration
    
    * add monitor fun to rebind to kz_nodes
    
    * remove listener in handle_down

--- src/mod_com_kazoo_monitor.erl
+++ src/mod_com_kazoo_monitor.erl
@@ -1,5 +1,5 @@
 %%%-----------------------------------------------------------------------------
-%%% @copyright (C) 2012-2020, 2600Hz
+%%% @copyright (C) 2012-2021, 2600Hz
 %%% @doc
 %%% This Source Code Form is subject to the terms of the Mozilla Public
 %%% License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -12,6 +12,8 @@
 -behaviour(gen_server).
 
 -export([start_link/0]).
+
+-export([monitor/0]).
 
 -export([init/1
         ,handle_call/3
@@ -68,6 +70,11 @@ handle_cast({'kz_nodes',{'heartbeat', #kz_node{zone = Zone} = Node}}, #{zone := 
 handle_cast({'kz_nodes',{'expire', #kz_node{zone = Zone} = Node}}, #{zone := Zone} = State) ->
     {'noreply', handle_expired(Node, State)};
 handle_cast({'kz_nodes', _Node}, State) ->
+    {'noreply', State};
+handle_cast(monitor_kz_nodes, State) ->
+    _ = kz_nodes:notify_new(),
+    _ = kz_nodes:notify_expire(),
+    _ = kz_nodes:notify_heartbeat(),
     {'noreply', State};
 handle_cast(_Cast, State) ->
     lager:debug("unhandled cast: ~p", [_Cast]),
@@ -211,12 +218,8 @@ maybe_node_up(#{is_node := false
     'ok' = ecallmgr_fs_nodes:add(Nodename, erlang:get_cookie(), [{'connect_strategy', 'heartbeat'}]),
     Context.
 
-maybe_node_sync(#{nodename := Nodename
-                 ,node_server := undefined
-                 } = Context) ->
-    lager:info("node server is down ~s", [Nodename]),
-    Context;
-maybe_node_sync(#{is_node := true
+maybe_node_sync(#{node_server := 'undefined'} = Context) -> Context;
+maybe_node_sync(#{is_node := 'true'
                  ,core_uuid := CoreUUID
                  ,nodename := Nodename
                  ,node_server := Server
@@ -234,12 +237,8 @@ maybe_node_sync(Context) -> Context.
 node_instance_uuid(Nodename) ->
     kz_term:to_atom(ecallmgr_fs_node:instance_uuid(Nodename), true).
 
-maybe_node_run(#{nodename := Nodename
-                ,node_server := undefined
-                } = Context) ->
-    lager:info("node server is down ~s", [Nodename]),
-    Context;
-maybe_node_run(#{is_node := true
+maybe_node_run(#{node_server := 'undefined'} = Context) -> Context;
+maybe_node_run(#{is_node := 'true'
                 ,nodename := Nodename
                 ,node := #kz_node{modules=Modules}
                 ,node_server := Server
@@ -305,3 +304,7 @@ mod_com_kazoo_code(Props) ->
      ++ [{'clause',1, [{'var',1,'X'}], [[{'call',1,{'atom',1,'is_atom'},[{'var',1,'X'}]}]], [{'var',1,'X'}]}]
      ++ [{'clause',1, [{'var',1,'_'}], [], [{'atom',1,'error_not_found'}]}]
     }.
+
+-spec monitor() -> ok.
+monitor() ->
+    gen_server:cast(?MODULE, monitor_kz_nodes).
