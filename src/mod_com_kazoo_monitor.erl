ed4e76e5b1666291dc594c61620824591a8b8a00
Luis Azedo <luis.azedo@factorlusitano.com>
2023-01-20 15:51:53 +0000

    KECE-4 use gen:stop/3 to restart dialplan fetch handler
    if a fast freeswitch restart occurs, a fs node may still be up for kazoo but a core-uuid changed and dialplan fetch needs restart.
    
    erlang:exit/2 was wrongly being used leading to route requests not beig handled because the dialplan fetch handlers are bound to the previous core-uuid

--- src/mod_com_kazoo_monitor.erl
+++ src/mod_com_kazoo_monitor.erl
@@ -1,5 +1,5 @@
 %%%-----------------------------------------------------------------------------
-%%% @copyright (C) 2012-2022, 2600Hz
+%%% @copyright (C) 2012-2023, 2600Hz
 %%% @doc
 %%% This Source Code Form is subject to the terms of the Mozilla Public
 %%% License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -262,7 +262,7 @@ sync_fetch(#{core_uuid := CoreUUID
             lager:warning("failed to get amqp fetch dialplan sup from ~s", [Nodename]),
             Context;
         Pid ->
-            erlang:exit(Pid, 'core_uuid_changed'),
+            _ = gen:stop(Pid, {shutdown, core_uuid_changed}, infinity),
             Context
     end.
 
